9d0e2022d7f798abd94d34b1e1e8a947318aa7b7
dalelantowork <dalelanto.work@gmail.com>
2021-04-08 18:21:01 +0800

    about us changed

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ export default function About() {
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        Lorem ipsum Made by Dale Lanto dolor sit amet consectetur adipisicing elit. Molestiae
         repudiandae architecto qui adipisci in officiis, aperiam sequi atque
         perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
         nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione
